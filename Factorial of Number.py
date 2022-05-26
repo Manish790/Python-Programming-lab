82bd0d8ab4563ddc758a7356b29e7a8e6b8cbf05
Manish <100483647+Manish790@users.noreply.github.com>
2022-05-26 21:40:42 +0530

    Update Factorial of Number.py

--- Factorial of Number.py
+++ Factorial of Number.py
@@ -1,6 +1,11 @@
 num=int(input())
 fact=1
-for i in range(1,n+1):
-  fact*=i
-print(fact)
+if num>1:
+  for i in range(1,n+1):
+    fact*=i
+  print(fact)
+elif num==0 or num==1:
+  print(1)
+else:
+  print("Invalid Number")
   
